5cdd003e39abb822e6c97af123db33c6f28cc5de
Chirag Choudhary <chiragchoudhary@Chirag-MacbookAir.local>
2017-02-07 13:13:02 +1000

    first commit

---
+++ spec/test-list-maker-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TestListMakerView from '../lib/test-list-maker-view';
+
+describe('TestListMakerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
